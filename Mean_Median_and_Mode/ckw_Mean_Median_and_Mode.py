a108e022fd4b747bc3bd1146623acb84e86b6828
clock509 <47171251+clock509@users.noreply.github.com>
2019-12-04 07:06:04 +0900

    Add files via upload

---
+++ Mean_Median_and_Mode/ckw_Mean_Median_and_Mode.py
@@ -0,0 +1,47 @@
+#1. 평균
+def Mean(n, x):
+    denominator = n
+    numerator = 0
+
+    for i in range(len(x)):
+        numerator += x[i]
+
+    return numerator / denominator
+
+#2. 중앙값
+def Median(n, x):
+    #먼저 오름차순 정렬
+    for i in range(len(x)-1):
+        min_idx = i
+        for j in range(i, len(x)):
+            if x[j] < x[min_idx]:
+                min_idx = j
+        x[i], x[min_idx] = x[min_idx], x[i]
+    
+    if n%2 == 0:
+        return (x[int(len(x)/2) - 1] + x[int(len(x)/2)]) / 2
+    else:
+        return x[int(len(x)/2)]
+
+#3. 
+def Mode(x):
+    n = len(x)
+    min_value = x[0]
+    for i in range(1, n):
+        if min_value > x[i]:
+            min_value = x[i]
+
+
+    for i in range(len(x)):
+        if x.count(x[i]) == x.count(x[0]):  #모든 원소가 같은 빈도일 시 원소 중 최소값 반환
+            return min_value
+        else:   #가장 많은 빈도의 원소가 존재하는 경우
+            return 
+
+
+n = 10
+x = [64630, 11735, 14216, 99233, 14470, 4978, 73429, 38120, 51135, 67060]
+
+print(Mean(n, x))
+print(Median(n, x))
+print(Mode(x))
